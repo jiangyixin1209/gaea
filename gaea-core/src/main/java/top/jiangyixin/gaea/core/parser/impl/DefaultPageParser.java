5f2d731cf6c325c0438d4771302d06878a4f4174
jiangyixin1209 <17602173915@163.com>
2020-12-22 14:37:09 +0800

    url manage

--- gaea-core/src/main/java/top/jiangyixin/gaea/core/parser/impl/DefaultPageParser.java
+++ gaea-core/src/main/java/top/jiangyixin/gaea/core/parser/impl/DefaultPageParser.java
@@ -1,10 +1,19 @@
 package top.jiangyixin.gaea.core.parser.impl;
 
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import top.jiangyixin.gaea.core.parser.PageParser;
+
 /**
  * TODO
  * @version 1.0
  * @author jiangyixin
  * @date 2020/12/22 下午2:17
  */
-public class DefaultPageParser {
+public class DefaultPageParser implements PageParser<Object> {
+	
+	@Override
+	public void parse(Document document, Element element, Object page) {
+	
+	}
 }
--- gaea-core/src/main/java/top/jiangyixin/gaea/core/runtime/impl/LocalUrlManage.java
+++ gaea-core/src/main/java/top/jiangyixin/gaea/core/runtime/impl/LocalUrlManage.java
@@ -1,10 +1,61 @@
 package top.jiangyixin.gaea.core.runtime.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import top.jiangyixin.gaea.core.exception.GaeaException;
+import top.jiangyixin.gaea.core.runtime.UrlManage;
+import top.jiangyixin.gaea.core.util.UrlUtil;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.LinkedBlockingDeque;
+
 /**
- * TODO
+ * 本地内存实现运行 Url管理
  * @version 1.0
  * @author jiangyixin
  * @date 2020/12/22 上午11:30
  */
-public class LocalRuntimeData {
+public class LocalUrlManage implements UrlManage {
+	
+	private final static Logger logger = LoggerFactory.getLogger(LocalUrlManage.class);
+	private final LinkedBlockingDeque<String> unVisitUrlQueue = new LinkedBlockingDeque<>();
+	private final Set<String> visitUrlSet = Collections.synchronizedSet(new HashSet<>());
+	
+	@Override
+	public boolean addUrl(String url) {
+		if (!UrlUtil.isUrl(url)) {
+			logger.info("Gaea LocalUrlManage.addUrl fail, url not valid: {}", url);
+			return false;
+		}
+		if (visitUrlSet.contains(url)) {
+			logger.info("Gaea LocalUrlManage.addUrl fail, url add visited: {}", url);
+			return false;
+		}
+		if (unVisitUrlQueue.contains(url)) {
+			logger.info("Gaea LocalUrlManage.addUrl fail, url add repeat: {}", url);
+			return false;
+		}
+		unVisitUrlQueue.add(url);
+		logger.info("Gaea LocalUrlManage.addUrl success, url: {}", url);
+		return true;
+	}
+	
+	@Override
+	public String getUrl() {
+		String url;
+		try {
+			url = unVisitUrlQueue.take();
+		} catch (InterruptedException e) {
+			throw new GaeaException("LocalUrlManage.getUrl interrupted");
+		}
+		visitUrlSet.add(url);
+		return url;
+	}
+	
+	@Override
+	public int getUrlNum() {
+		return unVisitUrlQueue.size();
+	}
 }
